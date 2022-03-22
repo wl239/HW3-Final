90754c313d532aad81070a11af1bb9bad155eb3d
Jake Vestal <jmvestal@gmail.com>
2022-03-22 12:39:31 -0400

    errors

--- fintech_ibkr/synchronous_functions.py
+++ fintech_ibkr/synchronous_functions.py
@@ -2,19 +2,29 @@
 from fintech_ibkr.ibkr_app import ibkr_app
 import threading
 import time
+from datetime import datetime
 
 # If you want different default values, configure it here.
 default_hostname = '127.0.0.1'
 default_port = 7497
 default_client_id = 10645 # can set and use your Master Client ID
+timeout_sec = 5
 
 def fetch_managed_accounts(hostname=default_hostname, port=default_port,
                            client_id=default_client_id):
 
     app = ibkr_app()
     app.connect(hostname, port, client_id)
+
+    start_time = datetime.now()
     while not app.isConnected():
         time.sleep(0.01)
+        if (start_time - datetime.now()).seconds > timeout_sec:
+            raise Exception(
+                "fetch_managed_accounts",
+                "timeout",
+                "couldn't connect to IBKR"
+            )
 
     def run_loop():
         app.run()
@@ -27,23 +37,44 @@ def fetch_managed_accounts(hostname=default_hostname, port=default_port,
     return app.managed_accounts
 
 def fetch_current_time(hostname=default_hostname,
-                          port=default_port, client_id=default_client_id):
+                       port=default_port, client_id=default_client_id):
     app = ibkr_app()
     app.connect(hostname, port, client_id)
+    start_time = datetime.now()
     while not app.isConnected():
         time.sleep(0.01)
+        if (start_time - datetime.now()).seconds > timeout_sec:
+            raise Exception(
+                "fetch_current_time",
+                "timeout",
+                "couldn't connect to IBKR"
+            )
 
     def run_loop():
         app.run()
 
     api_thread = threading.Thread(target=run_loop, daemon=True)
     api_thread.start()
+    start_time = datetime.now()
     while isinstance(app.next_valid_id, type(None)):
         time.sleep(0.01)
+        if (start_time - datetime.now()).seconds > timeout_sec:
+            raise Exception(
+                "fetch_current_time",
+                "timeout",
+                "next_valid_id not received"
+            )
 
     app.reqCurrentTime()
+    start_time = datetime.now()
     while isinstance(app.current_time, type(None)):
         time.sleep(0.01)
+        if (start_time - datetime.now()).seconds > timeout_sec:
+            raise Exception(
+                "fetch_current_time",
+                "timeout",
+                "current_time not received"
+            )
     app.disconnect()
     return app.current_time
 
@@ -54,40 +85,86 @@ def fetch_historical_data(contract, endDateTime='', durationStr='30 D',
                           port=default_port, client_id=default_client_id):
     app = ibkr_app()
     app.connect(hostname, port, client_id)
+    start_time = datetime.now()
     while not app.isConnected():
         time.sleep(0.01)
+    if (start_time - datetime.now()).seconds > timeout_sec:
+        raise Exception(
+            "fetch_historical_data",
+            "timeout",
+            "couldn't connect to IBKR"
+        )
+
     def run_loop():
         app.run()
     api_thread = threading.Thread(target=run_loop, daemon=True)
     api_thread.start()
+    start_time = datetime.now()
     while isinstance(app.next_valid_id, type(None)):
         time.sleep(0.01)
+        if (start_time - datetime.now()).seconds > timeout_sec:
+            raise Exception(
+                "fetch_historical_data",
+                "timeout",
+                "next_valid_id not received"
+            )
     tickerId = app.next_valid_id
     app.reqHistoricalData(
         tickerId, contract, endDateTime, durationStr, barSizeSetting,
         whatToShow, useRTH, formatDate=1, keepUpToDate=False, chartOptions=[])
+    start_time = datetime.now()
     while app.historical_data_end != tickerId:
         time.sleep(0.01)
+        if (start_time - datetime.now()).seconds > timeout_sec:
+            raise Exception(
+                "fetch_historical_data",
+                "timeout",
+                "historical_data not received"
+            )
     app.disconnect()
     return app.historical_data
 
 def fetch_contract_details(contract, hostname=default_hostname,
-                          port=default_port, client_id=default_client_id):
+                           port=default_port, client_id=default_client_id):
     app = ibkr_app()
     app.connect(hostname, port, client_id)
+    start_time = datetime.now()
     while not app.isConnected():
         time.sleep(0.01)
+        if (start_time - datetime.now()).seconds > timeout_sec:
+            raise Exception(
+                "fetch_contract_details",
+                "timeout",
+                "couldn't connect to IBKR"
+            )
 
     def run_loop():
         app.run()
 
     api_thread = threading.Thread(target=run_loop, daemon=True)
     api_thread.start()
+    start_time = datetime.now()
     while isinstance(app.next_valid_id, type(None)):
         time.sleep(0.01)
+        if (start_time - datetime.now()).seconds > timeout_sec:
+            raise Exception(
+                "fetch_contract_details",
+                "timeout",
+                "next_valid_id not received"
+            )
+
     tickerId = app.next_valid_id
     app.reqContractDetails(tickerId, contract)
+
+    start_time = datetime.now()
     while app.contract_details_end != tickerId:
         time.sleep(0.01)
+        if (start_time - datetime.now()).seconds > timeout_sec:
+            raise Exception(
+                "fetch_contract_details",
+                "timeout",
+                "contract_details not received"
+            )
+
     app.disconnect()
     return app.contract_details
